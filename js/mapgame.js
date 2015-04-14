b427c399fca3e98d7d125447b66737e02e9061f5
Liz Silich <lsilich04@gmail.com>
2015-04-14 17:01:16 -0500

    more setup
    startGame function started by a dialog box

--- js/mapgame.js
+++ js/mapgame.js
@@ -1,6 +1,25 @@
 //mapgame.js
 
 $(document).ready(function () {
+
+	// establishing jqueryui elements
 	$( ".uiselectmenu" ).selectmenu();
 	$( ".uibutton" ).button();
+	$( ".ui-dialog").dialog({
+		buttons: {
+				"Let's Go!": function () {
+					$(this).dialog("close");
+					startGame();
+				}
+		},
+	});
+	
+	// actual actions
+	$("#dialogbox").dialog("open");	
 });
+
+function startGame () {
+	// called from the dialog close button!
+	// put some gmaps stuff here probably?
+	console.log("Check!");
+}
