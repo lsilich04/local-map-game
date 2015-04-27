a8a430607f25ce643769734c91b58550757f6134
Liz Silich <lsilich04@gmail.com>
2015-04-27 11:19:11 -0500

    we did pretty much everything

--- js/mapgame.js
+++ js/mapgame.js
@@ -1,26 +1,55 @@
 //mapgame.js
+var score = 100;
+var correct = 0;
+var total = 0;
+var wrong = 0;
 
 $(document).ready(function () {
-
 	// establishing jqueryui elements
-	$( ".dialog").dialog({
+	$( "#dialogbox").dialog({
 		buttons: {
-				"Let's Go!": function () {
-					$(this).dialog("close");
-					startGame();
-				}
+			"Let's Go!": function () {
+			$(this).dialog("close");
+			startGame();
+			}
 		},
 	});
+	$("#dialogloser").dialog({
+		buttons: {
+			"Play again?": function () {
+				restart();
+			}
+		}
+	});
+	$("#dialogwinner").dialog({
+		buttons: {
+			"Play again?": function () {
+				restart();
+			}
+		}});
 	$(".button").button();
 	
 	// actual actions
 	$("#dialogbox").dialog("open");	
+	$("#dialogloser").dialog("close");
+	$("#dialogwinner").dialog("close");
 	$("#restart").click(function () {
-    initialize();
-		$("#dialogbox").dialog("open");
+		restart();
 	});
-	
+
 });
+
+function restart () {
+	initialize();
+	score = 100;
+	correct = 0;
+	total = 0;
+	wrong = 0;
+	$("#dialogloser").dialog("close");
+	$("#dialogwinner").dialog("close");
+	$("#hp").css("width", score + "%");
+	$("#dialogbox").dialog("open");
+}
 
 function startGame () {
 	performSearch();
@@ -29,17 +58,35 @@ function startGame () {
 var map;
 var infoWindow;
 var service;
+var diff;
+var str = "";
 
 function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 16,
-    center: new google.maps.LatLng(41.6564208,-91.5332455)
+    center: new google.maps.LatLng(41.6564208,-91.5332455),
+	styles : [
+			{
+				stylers: {visibility: 'simplified'}
+			},
+			{
+				elementType: 'labels',
+				stylers: [
+					{visibility: 'off'}
+				]
+			},
+			{
+				featureType: 'road',
+				elementType: 'labels',
+				stylers: [
+					{visibility: 'on'}
+				]
+			}
+		]
   });
 
   infoWindow = new google.maps.InfoWindow();
   service = new google.maps.places.PlacesService(map);
-
-  //google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
 }
 
 function performSearch() {
@@ -57,8 +104,18 @@ function callback(results, status) {
     return;
   }
   for (var i = 0, result; result = results[i]; i++) {
+   total++;
     createMarker(result);
   }
+	diff = Math.ceil(total/15);
+	if (diff <= 0) diff = 1;
+	if (diff >= 6) diff = 5;
+	str = "";
+	for (var i=0; i < diff ;i++) {
+		str += "&#x2605;";
+	}
+	document.getElementById("difficulty").innerHTML = ' Difficulty: <span id="stars">' + str + "</span>";
+	document.getElementById("scorer").innerHTML = "Score: " + correct + "/" + total;
 }
 
 function createMarker(place) {
@@ -80,19 +137,40 @@ function createMarker(place) {
 
   google.maps.event.addListener(marker, 'click', function() {
    service.getDetails(place, function(result, status) {
-      if (status != google.maps.places.PlacesServiceStatus.OK) {
-        alert(status);
-        return;
-      }
+      
       var guess= document.getElementById('guess');
-      infoWindow.setContent(result.name);
-      if(result.name!=guess.value){
-        alert('oh, you are so wrong.');
-      }
-      if(result.name==guess.value){
-        alert('so proud. you right');
-      }
-      infoWindow.open(map, marker);
+	  if (guess.value.length < 4) {
+		$("#charlength").css("opacity", "1");
+	  } else {
+		$("#charlength").css("opacity", "0");
+		  if (status != google.maps.places.PlacesServiceStatus.OK) {
+			alert(status);
+			return;
+		  }
+		  infoWindow.setContent(result.name);
+		  if (result.name.toLowerCase().indexOf(guess.value.toLowerCase()) >= 0){
+			correct++;
+			$("#right").css("opacity", "1");
+			setTimeout(function() { $("#right").css("opacity", "0"); }, 2000);
+		  } else {
+			score -= 20;
+			wrong++;
+			$("#wrong").css("opacity", "1");
+			setTimeout(function() { $("#wrong").css("opacity", "0"); }, 2000);
+			$("#hp").css("width", score + "%");
+			if (score < 1) {
+				$("#dialogloser").dialog("open");
+				setTimeout( function () {$("#hpbar").effect("shake", {direction: "up", distance: 10});}, 1000);
+			}
+		  }
+		  infoWindow.open(map, marker);
+		  document.getElementById("scorer").innerHTML = "Score: " + correct + "/" + total;
+		  if (correct >= (total-wrong)){
+			$("#dialogwinner").dialog("open");
+		  }
+		  marker.setMap(null);
+	  
+	  }
 	  
     }); 
   });
